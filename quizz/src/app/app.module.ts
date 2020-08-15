d6f13bb9772c6f0de11ea2d25f4e15b69d06a27d
unknown <susansarathomas98@gmail.com>
2020-08-15 12:53:49 +0530

    first commit

---
+++ quizz/src/app/app.module.ts
@@ -0,0 +1,52 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { UserService } from './user.service';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TakequizComponent } from './takequiz/takequiz.component';
+import { ReviewComponent } from './review/review.component';
+import { ResultsComponent } from './results/results.component';
+import { SubmitquizComponent } from './submitquiz/submitquiz.component';
+import {FormsModule} from '@angular/forms'
+import { HttpClientModule ,HTTP_INTERCEPTORS } from '@angular/common/http';
+import { QuizService } from './quiz.service';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { SavingComponent } from './saving/saving.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { IcinBankComponent } from './icin-bank/icin-bank.component';
+import { MyprofileComponent } from './myprofile/myprofile.component';
+import { AdminhomeComponent } from './adminhome/adminhome.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { ChequerequestComponent } from './chequerequest/chequerequest.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TakequizComponent,
+    ReviewComponent,
+    ResultsComponent,
+    SubmitquizComponent,
+    ProfileComponent,
+    SavingComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    IcinBankComponent,
+    MyprofileComponent,
+    AdminhomeComponent,
+    UserdetailsComponent,
+    ChequerequestComponent,
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [UserService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ quizz/src/app/chequerequest/chequerequest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chequerequest',
+  templateUrl: './chequerequest.component.html',
+  styleUrls: ['./chequerequest.component.css']
+})
+export class ChequerequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
